daff045d12fe7be2dd5588cacb84c807b10291fc
Tom Fournier <tom.fournier@outlook.com>
2026-07-08 14:13:36 -0500

    Initial Commit

---
+++ Sorriva/SorrivaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SorrivaApp.swift
+//  Sorriva
+//
+//  Created by Thomas Fournier on 7/8/26.
+//
+
+import SwiftUI
+
+@main
+struct SorrivaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
